57e4e668761bfc428bfbcef403e75659dbd882c8
Jhonnye <43444151+jhonnye0@users.noreply.github.com>
2019-05-08 17:44:42 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class Main
 {
-    public static Employee add()
+    public static Employee add(String name, String address, String type, String attribute, int id)
     {
         Scanner input = new Scanner(System.in);
-        Employee newEmp = new Employee(input.nextLine(), input.nextLine(), input.nextLine(), input.nextLine(), input.nextInt());
+        Employee newEmp = new Employee(name, address, type, attribute, id);
         return newEmp;
     }
 
@@ -13,22 +13,52 @@ public class Main
     {
         Scanner input = new Scanner(System.in);
 
-        Employee[] List = new Employee[100];
+        Employee[] list = new Employee[100];
 
-        int quantity = 0;
         while(1 == 1)
         {
+            System.out.println("Digite a operação que deseja:\n" +
+                    "0. Finalizar\n" +
+                    "1. Adcionar empregado\n" +
+                    "2. Remover empregado\n" +
+                    "6. Reajustar informações de um empregado\n");
+
             int operation = input.nextInt();
-            if(operation == -1) break;
+            int id;
+            if(operation == 0) break;
 
             switch(operation)
             {
                 case 1:
-                    add();
+                    int i;
+                    for(i = 0; i<100; i++)
+                    {
+                        if(list[i] == null) break;
+                    }
+                    id = i;
+
+                    System.out.print("Digite o nome:\n");
+                    String name = input.nextLine();
+                    System.out.print("Endereço:\n");
+                    String address = input.nextLine();
+                    System.out.println("Tipo de empregado:");
+                    String type = input.nextLine();
+                    System.out.println("Atributos do empregado:");
+                    String attribute = input.nextLine();
+
+                    list[id] = add(name, address, type, attribute, id);
+                    break;
                 case 2:
-                    //remove();
+                    id = input.nextInt();
+                    //remove(id);
+                    break;
                 case 6:
-                    //update();
+                    id = input.nextInt();
+                    //update(id);
+                    break;
+                default:
+                    System.out.println("Por favor digite novamente o número da operação específica");
+                    break;
             }
         }
     }

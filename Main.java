6d7f4d4400bb7d8cf310159840ed3683ab627e2e
Jhonnye <43444151+jhonnye0@users.noreply.github.com>
2019-05-08 16:45:03 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static Employee add()
+    {
+        Scanner input = new Scanner(System.in);
+        Employee newEmp = new Employee(input.nextLine(), input.nextLine(), input.nextLine(), input.nextLine(), input.nextInt());
+        return newEmp;
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int quantity = 0;
+        while(1 == 1)
+        {
+            int operation = input.nextInt();
+            if(operation == -1) break;
+
+            switch(operation)
+            {
+                case 1:
+                    add();
+                case 2:
+                    //remove();
+                case 6:
+                    //update();
+            }
+        }
+    }
+}

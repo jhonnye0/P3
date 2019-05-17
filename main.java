4629e6ac438566b20d50babd2524fe8bdf60892b
Jhonnye <43444151+jhonnye0@users.noreply.github.com>
2019-05-17 16:52:33 -0300

    Update main.java

--- main.java
+++ main.java
@@ -1,31 +1,27 @@
 import java.util.Scanner;
 
-/*
-0. id
-1. name
-2. adress
-3. type
-4. syndicate
-5. synID
-6. paymentMethod
-7. salary
-8. payday
-9. frequence
-10. paymentWeekDay
- */
-
 public class Main
 {
     private static int DIA = 1;
     private static int HORA = 0;
 
+    static String[] names = new String[100];
+    static String[] adress = new String[100];
+    static int[] types = new int[100];
+    static double[] salary = new double[100];
+    static int[] Syndicato = new int[1000];
+    static boolean[] synd = new boolean[100];
+    static double[][] vendas = new double[100][31];
+    static int[] ponto = new int[100];
+    static int[] paymentMethod = new int[100];
+    static int[] synID = new int[100];
+    static int[] payday = new int[100];
+    static int[] frequence = new int[100];
+    static int[] paymentWeekDay = new int[100];
+    static double[] fundo = new double[100];
+
     public static void main(String[] args)
     {
-        String[][] lista = new String[100][11];
-        int[] Syndicate = new int[100];
-        double[][] vendas = new double[100][30];
-        int[] ponto = new int[100];
-
         Scanner input = new Scanner(System.in);
         while(true)
         {
@@ -34,8 +30,9 @@ public class Main
                     "0. Finalizar\n" +
                     "1. Adcionar empregado\n" +
                     "2. Remover empregado\n" +
-                    "3. Marcar o Ponto\n" +
-                    "4. Registrar venda\n" +
+                    "3. Registrar venda\n" +
+                    "4. Marcar ponto\n" +
+                    "5. Lançar taxa de serviço\n" +
                     "6. Reajustar informações de um empregado\n");
             int operation = input.nextInt();
             input.nextLine();
@@ -47,16 +44,14 @@ public class Main
                     int i;
                     for(i = 0; i<100; i++)
                     {
-                        if(lista[i][0] == null) break;
+                        if(names[i] == null) break;
                     }
-                    lista[i][0] = String.valueOf((int)i);
-                    add(i, lista, Syndicate);
+                    add(i);
                     break;
                 case 2:
                     System.out.print("Digite o ID do empregado que deseja remover:\n");
                     id = input.nextInt();
-                    lista[id][0] = null;
-                    Syndicate[id] = 0;
+                    remove(id);
                     break;
                 case 3:
                     System.out.print("Digite seu ID:\n");
@@ -74,14 +69,37 @@ public class Main
                     }
                     else
                     {
-                        int cal = HORA - ponto[id];
-                        if(cal > 8 && Integer.parseInt(lista[id][3]) == 1)
+                        int cont = 0;
+                        for (i = ponto[id]; true; i++)
                         {
-                            lista[id][7] = String.valueOf(Double.parseDouble(lista[id][7]) + (cal - 8)*1.5);
+                            if(i == HORA) break;
+                            cont++;
+                            i = i%24;
+                        }
 
+                        if(cont > 8 && types[id] == 1)
+                        {
+                            fundo[id] = salary[id] + (cont - 8)*1.5*salary[id];
                         }
                     }
+                    System.out.println("Horários registrado!");
                     break;
+                case 5:
+                    System.out.print("Digite seu ID:\n");
+                    id = input.nextInt();
+                    if(names[id] == null)
+                    {
+                        System.out.print("O empregado não existe\n");
+                    }
+                    else if(synd[id] == true)
+                    {
+                        System.out.print("Insira a taxa de serviço:\n");
+                        fundo[id] -= input.nextDouble();
+                    }
+                    else
+                        System.out.print("Você nao pertence ao sindicato\n");
+                    break;
+
                 default:
                     System.out.print("Digite novamente..\n");
                     break;
@@ -89,42 +107,42 @@ public class Main
         }
     }
 
-    private static void add(int id, String[][] lista, int[] syndicate)
+    private static void add(int id)
     {
         Scanner input = new Scanner(System.in);
 
         System.out.print("Digite o nome:\n");
-        lista[id][1] = input.nextLine();
+        names[id] = input.nextLine();
         System.out.print("Endereço:\n");
-        lista[id][2] = input.nextLine();
+        adress[id] = input.nextLine();
         System.out.print("Tipo de empregado:\n" +
                     "1. Horista\n" +
                     "2. Comissionado\n" +
                     "3. Assalariado\n");
-        lista[id][3] = String.valueOf(input.nextInt());
+        types[id] = input.nextInt();
         input.nextLine();
 
         System.out.println("Atributos default:\n");
-        if(Integer.parseInt(lista[id][3]) == 1)
+        if(types[id] == 1)
         {
-            lista[id][9] = String.valueOf((int) 1);
-            lista[id][10] = String.valueOf((int) 6);
+            frequence[id] = 1;
+            paymentWeekDay[id] = 6;
             System.out.println("Pagamento 1 vez por semana\n" +
                         "Dia de pagamento na sexta-feira.\n\n" +
                         "Pode ser alterado futuramente..\n");
 
         }
-        else if(Integer.parseInt(lista[id][3]) == 2)
+        else if(types[id] == 2)
         {
-            lista[id][9] = String.valueOf((int) 2);
-            lista[id][10] = String.valueOf((int) 6);
+            frequence[id] = 2;
+            paymentWeekDay[id] = 6;
             System.out.println("Pagamento 1 vez por semana\n" +
                         "Dia de pagamento na sexta-feira.\n\n" +
                         "Pode ser alterado futuramente..\n");
         }
         else
         {
-            lista[id][8] = String.valueOf((int) 31);
+            payday[id] = 31;
             System.out.print("Pagamento 1 vez por mês\n" +
                     "Dia de pagamento no último dia do mês\n" +
                     "Dia: 30\n" +
@@ -135,32 +153,70 @@ public class Main
                         "1. Cheque pelos correios\n" +
                         "2. Cheque em mãos\n" +
                         "3. Depósito na conta bancária\n");
-        lista[id][6] = String.valueOf((int)input.nextInt());
+        paymentMethod[id] = input.nextInt();
         input.nextLine();
 
         System.out.print("Qual o salário do empregado?\n");
-        lista[id][7] = String.valueOf((double)input.nextDouble());
+        salary[id] = input.nextDouble();
         input.nextLine();
+
+        System.out.printf("\nSeu ID na empresa é: |%d|\n", id);
 
         System.out.print("\nGostaria de participar do sindicato?\n" +
                     "y - sim\n" +
                     "n - não\n");
+
         if(input.nextLine().intern() == "y")
         {
-            lista[id][4] = String.valueOf((int) 1);
+            synd[id] = true;
             int i;
-            for(i = 0; i<100; i++)
+            for(i = 999; i>=0; --i)
             {
-                if (syndicate[i] == 0) break;
+                if (Syndicato[i] == 0)
+                {
+                    Syndicato[i] = 1;
+                    break;
+                }
             }
-            lista[id][5]= String.valueOf((int)i);
-            System.out.printf("\nSeu ID na empresa é: |%d|\n", Integer.parseInt(lista[id][0]));
-            System.out.printf("Seu ID no sindicato é: |%d|\n", Integer.parseInt(lista[id][5]));
-            System.out.print("Registrado com sucesso.\n");
-            System.out.print("Lembre-se o seu ID é muito importante para acessar suas informações guarde-o!!\n");
-
+            synID[id] = i;
+            System.out.print("A taxa sindical no valor (default) de 10 reais será descontado do seu salário ao fim do mês\n");
+            System.out.printf("Seu ID no sindicato é: |%d|\n", synID[id]);
         }
         else
-            lista[id][4] = String.valueOf((int) 0);
+            synd[id] = false;
+
+        System.out.print("Registrado com sucesso.\n");
+        System.out.print("Lembre-se os seus ID's são muito importantes para acessar suas informações guarde-o!!\n");
+    }
+
+    private static void remove(int id)
+    {
+        if(names[id] == null) 
+        {
+            System.out.println("O empregado não está registrado\n");
+            return;
+        }
+        int i,j;
+        names[id] = null;
+        adress[id] = null;
+        types[id] = 0;
+        salary[id] = 0;
+        Syndicato[id] = 0;
+        synd[id] = false;
+        for(i = 0; i<100; i++)
+        {
+            for (j=0; j<31; j++)
+            {
+                vendas[i][j] = 0;
+            }
+        }
+        ponto[id] = 0;
+        paymentMethod[id] = 0;
+        synID[id] = 0;
+        payday[id] = 0;
+        frequence[id] = 0;
+        paymentWeekDay[id] = 0;
+        fundo[id] = 0;
+        System.out.println("Empregado removido com sucesso!");
     }
 }
